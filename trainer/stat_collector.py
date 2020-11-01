db80a38b0a0970259743d30ec90b43aedd54c632
Ryu0w0 <flymetothemoon0w0@gmail.com>
2020-11-01 17:09:27 +0900

    no message

---
+++ trainer/stat_collector.py
@@ -0,0 +1,38 @@
+import numpy as np
+from sklearn import metrics
+from utils.logger import logger_, writer_
+
+
+class StatCollector:
+    def __init__(self, class_names, args):
+        self.class_names = class_names
+        self.args = args
+
+    def logging_stat_cae(self, mode, cur_epoch, cur_fold, mean_loss):
+        logger_.info(f"[{cur_fold}/{self.args.num_folds}][{cur_epoch}/{self.args.num_epoch}] "
+                     f"{mode} loss: {np.round(mean_loss, 4)}")
+        writer_.add_scalars(main_tag=f"{mode}/loss",
+                            tag_scalar_dict={f"fold{cur_fold}": mean_loss},
+                            global_step=cur_epoch)
+
+    def calc_stat_cnn(self, total_loss, preds, labels):
+        mean_loss = total_loss / len(preds)
+        stats = metrics.classification_report(labels, preds, target_names=self.class_names, output_dict=True)
+        return mean_loss, stats
+
+    def logging_stat_cnn(self, mode, cur_epoch, cur_fold, stats, mean_loss):
+        # logging overall loss and acc
+        for stat_nm, stat in zip(["loss", "acc"], [mean_loss, stats["accuracy"]]):
+            writer_.add_scalars(main_tag=f"{mode}/{stat_nm}",
+                                tag_scalar_dict={f"fold{cur_fold}": stat},
+                                global_step=cur_epoch)
+        logger_.info(f"[{cur_fold}/{self.args.num_folds}][{cur_epoch}/{self.args.num_epoch}] "
+                     f"{mode} loss: {np.round(mean_loss, 4)}, {mode} acc: {np.round(stats['accuracy'], 4)}")
+
+        # logging precision, recall and f1-score per class
+        for cls_nm, stat in stats.items():
+            if cls_nm in self.class_names:
+                for stat_type in ["precision", "recall", "f1-score"]:
+                    writer_.add_scalars(main_tag=f"{mode}_fold{cur_fold}/{stat_type}",
+                                        tag_scalar_dict={f"{cls_nm}": stat[stat_type]},
+                                        global_step=cur_epoch)
